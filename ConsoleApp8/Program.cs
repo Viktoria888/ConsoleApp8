14958ba98bc21650d710066a540941249811f463
nata-vika@list.ru <nata-vika@list.ru>
2020-02-09 15:59:18 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class First
+    {
+
+        public int a = 3;
+        public int b = 2;
+
+        public void MyInfo()
+        {
+            Console.WriteLine($"Переменная 1: {a}, Переменная 2: {b}");
+            Console.Write("Введите два числа, чтобы изменить значения переменных ");
+        }
+
+    }
+    public class Second
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        public void Change()
+        {
+            Console.WriteLine($"Переменная 1: {num1},Переменная 2: {num2}");
+        }
+        public void Sum()
+        {
+            Console.WriteLine($"Сумма: {num1 + num2}");
+        }
+        public void More()
+        {
+          
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Наибольшее значение: {num1}");
+            }
+            else
+            {
+                Console.WriteLine($"Наибольшее значение: {num2}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            First one = new First();
+            one.MyInfo();
+
+            Second two = new Second();
+            two.Change();
+            two.Sum();
+            two.More();
+
+            Console.ReadKey();
+        }
+    }
+}
